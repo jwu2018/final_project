14ccd21a274d5e52104874c868fa01ac4d238139
jwu2018 <jyalu.wu@hotmail.com>
2021-10-10 10:10:45 -0400

    suicide hotlines map + action plans by country

---
+++ frontend/src/data/geo/suicide_hotlines.js
@@ -0,0 +1,122 @@
+export default [
+    {
+        "id": "ARG",
+        "value": 781001
+    },
+    {
+        "id": "AUS",
+        "value": 781001
+    },
+    {
+        "id": "AUT",
+        "value": 781001
+    },
+    {
+        "id": "BEL",
+        "value": 781001
+    },
+    {
+        "id": "BWA",
+        "value": 781001
+    },
+    {
+        "id": "BRA",
+        "value": 781001
+    },
+    {
+        "id": "CAN",
+        "value": 781001
+    },
+    {
+        "id": "CHN",
+        "value": 781001
+    },
+    {
+        "id": "HRV",
+        "value": 781001
+    },
+    {
+        "id": "DNK",
+        "value": 781001
+    },
+    {
+        "id": "EGY",
+        "value": 781001
+    },
+    {
+        "id": "FIN",
+        "value": 781001
+    },
+    {
+        "id": "FRA",
+        "value": 781001
+    },
+    {
+        "id": "DEU",
+        "value": 781001
+    },
+    {
+        "id": "NLD",
+        "value": 781001
+    },
+    {
+        "id": "IND",
+        "value": 781001
+    },
+    {
+        "id": "ITA",
+        "value": 781001
+    },
+    {
+        "id": "IRL",
+        "value": 781001
+    },
+    {
+        "id": "JPN",
+        "value": 781001
+    },
+    {
+        "id": "MEX",
+        "value": 781001
+    },
+    {
+        "id": "NZL",
+        "value": 781001
+    },
+    {
+        "id": "NOR",
+        "value": 781001
+    },
+    {
+        "id": "PHL",
+        "value": 781001
+    },
+    {
+        "id": "POL",
+        "value": 781001
+    },
+    {
+        "id": "RUS",
+        "value": 781001
+    },
+    {
+        "id": "ESP",
+        "value": 781001
+    },
+    {
+        "id": "ZAF",
+        "value": 781001
+    },
+    {
+        "id": "SWE",
+        "value": 781001
+    },
+    {
+        "id": "GBR",
+        "value": 781001
+    },
+    {
+        "id": "USA",
+        "value": 781001
+    }
+]
---
+++ frontend/src/data/prevention/action_plans.js
@@ -0,0 +1,44 @@
+export default [
+    {
+        "region": "African Region",
+        "existing plans": 0,
+        "existing plansColor": "hsl(262, 70%, 50%)",
+        "developing plans": 3,
+        "developing plansColor": "hsl(329, 70%, 50%)"
+    },
+    {
+        "region": "Region of the Americas",
+        "existing plans": 8,
+        "existing plansColor": "hsl(262, 70%, 50%)",
+        "developing plans": 2,
+        "developing plansColor": "hsl(329, 70%, 50%)"
+    },
+    {
+        "region": "Eastern Mediterranean Region",
+        "existing plans": 0,
+        "existing plansColor": "hsl(262, 70%, 50%)",
+        "developing plans": 1,
+        "developing plansColor": "hsl(329, 70%, 50%)"
+    },
+    {
+        "region": "European Region",
+        "existing plans": 13,
+        "existing plansColor": "hsl(262, 70%, 50%)",
+        "developing plans": 5,
+        "developing plansColor": "hsl(329, 70%, 50%)"
+    },
+    {
+        "region": "South-East Asia Region",
+        "existing plans": 2,
+        "existing plansColor": "hsl(262, 70%, 50%)",
+        "developing plans": 0,
+        "developing plansColor": "hsl(329, 70%, 50%)"
+    },
+    {
+        "region": "Western Pacific",
+        "existing plans": 5,
+        "existing plansColor": "hsl(262, 70%, 50%)",
+        "developing plans": 2,
+        "developing plansColor": "hsl(329, 70%, 50%)"
+    }
+]
