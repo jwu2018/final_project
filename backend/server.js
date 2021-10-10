fb25719843ad81430207a9d2393bdfd337e19b59
jwu2018 <jyalu.wu@hotmail.com>
2021-10-10 18:47:56 -0400

    connected to database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express'),
+    mongodb = require( 'mongodb' ),
     app = express(),
     cors = require('cors'),
     dreams = [];
@@ -15,6 +16,45 @@ app.use(cors({
     origin: 'http://localhost'
 }))
 
+/**--------------------------------------------
+ *               MONGODB
+ *---------------------------------------------**/
+ const db_name = 'survey',
+ db_collection = 'answers'
+
+const uri = 'mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASS+'@'+process.env.DB_HOST
+
+const client = new mongodb.MongoClient( uri, { useNewUrlParser: true, useUnifiedTopology:true })
+let collection = null
+let users_collection = null
+
+
+client.connect()
+.then( () => {
+// will only create collection if it doesn't exist
+return client.db( db_name ).collection( db_collection)
+})
+.then( __collection => {
+// store reference to collection
+collection = __collection
+// blank query returns all documents
+// return collection.find({ }).toArray()
+})
+.then( console.log('connected to database') )
+
+// check connection
+app.use( (req,res,next) => {
+if( collection !== null ) {
+next()
+}else{
+res.status( 503 ).send()
+}
+})
+
+
+/**--------------------------------------------
+ *               EXPRESS ROUTES
+ *---------------------------------------------**/
 app.get('/dreams', (req, res) => {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify(dreams));
