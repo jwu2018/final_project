7e4751d913611aa0f2d51d393c0923dc0f8faa24
jwu2018 <jyalu.wu@hotmail.com>
2021-10-10 18:52:30 -0400

    get and post requests

--- backend/server.js
+++ backend/server.js
@@ -60,10 +60,67 @@ app.get('/dreams', (req, res) => {
     res.end(JSON.stringify(dreams));
 })
 
-app.post('/submit', function(request, response) {
-    dreams.push(request.body.newdream);
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify(dreams));
+app.use( function( req, res, next ) {
+    console.log( 'url:', req.url )
+    if( req.session.login === true )
+      next()
+    else
+      res.sendFile( __dirname + '/public/views/login.html' )
+  })
+  
+app.get('/', function(req, res) {
+    res.sendFile( __dirname + '/public/views/index.html' )
+})
+
+app.get( '/getAnswers', function (req, res) {
+    res.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+    if( collection !== null ) {
+      // get array and pass to res.json
+      collection.find({ }).toArray()
+      .then(result => res.end( JSON.stringify(result)))
+      .then( json => {
+        return json
+      })
+    }
+})
+
+// app.post('/submit', function(request, response) {
+//     dreams.push(request.body.newdream);
+//     response.writeHead(200, { 'Content-Type': 'application/json' });
+//     response.end(JSON.stringify(dreams));
+// })
+
+app.use('/submit',  function( request, response, next ) {
+    console.log('submitting...')
+    let dataString = ''
+  
+    request.on( 'data', function( data ) {
+      dataString += data 
+    })
+  
+    request.on( 'end', function() {
+      console.log(json)
+  
+      // history.push( json )
+      // debugger
+      collection.insertOne( json )//.then( result => response.json( result ) )
+      // add a 'json' field to our request object
+      request.json = JSON.stringify( json )
+      next()
+    })
+  })
+  
+  
+app.post( '/submit', function( request, response ) {
+    // our request object now has a 'json' field in it from our
+    // previous middleware
+    // history.push( request.body.newmix)
+    // console.log('response', response)
+    // debugger
+    // console.log('submit', request.json)
+    response.writeHead( 200, { 'Content-Type': 'application/json'})
+    // console.log('submit response', request.json )
+    response.end( JSON.stringify(request.json ))
 })
 
 const listener = app.listen(process.env.PORT, function() {
